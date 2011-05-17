8a2c4324e9280addbb01c31cab2c657bf0bbba53
Troy Will <troydwill@gmail.com>
2011-05-17 06:26:11 -0700

    modified:   pass1.pl

--- usr/local/bin/pass1.pl
+++ usr/local/bin/pass1.pl
@@ -29,7 +29,7 @@ sub display_01 {
     chomp( my $date = `date`);
     print "+---------- Available Wireless Networks $date -----------------------+\n\n";
 #    foreach my $mac ( sort keys %HoC ) {
-    foreach my $mac ( sort keys %HoC ) {
+    foreach my $mac ( sort { keys %{$HoC{$b}} <=> keys %{$HoC{$a}} } keys %HoC ) {
 	print "\t", $counter++, " $mac $HoC{$mac}{ESSID}";
 	if ( $HoC{$mac}{Encryption} eq 'off' ) {
 	    print color 'bold green';
