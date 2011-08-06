238a7279e86fcc7e3c6851ba8316eb8ae49ad306
Troy Will <troydwill@gmail.com>
2011-08-06 11:35:00 -0700

    10-stow-ruby.sh: --bindir

--- usr/local/bin/shiloh-setup/050-install-devel-base/10-stow-ruby.sh
+++ usr/local/bin/shiloh-setup/050-install-devel-base/10-stow-ruby.sh
@@ -2,8 +2,7 @@
 set -o errexit
 set -o nounset
 
-./configure --prefix=/usr/local
+./configure --prefix=/usr/local --bindir=/usr/local/bin/ruby-1.9.2-p290
 make
 make test && sudo make DESTDIR=/stow/ruby-1.9.2-p290 install && cd /stow/ \
     && tar -cjvf ruby-1.9.2-p290.stow.tar.bz2
-
