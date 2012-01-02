875643e00ce860d8692c9773f469bc7996eb0ea2
Troy Will <troydwill@gmail.com>
2012-01-01 20:00:57 -0800

    02-stow-ruby.sh: change man dir so that it doesn't conflict with stow

--- 06-install-ruby/02-stow-ruby.sh
+++ 06-install-ruby/02-stow-ruby.sh
@@ -4,7 +4,9 @@ set -o nounset
 
 RUBYVERSION="1.9.3-p0"
 
-./configure --prefix=/usr/local --bindir=/usr/local/bin/ruby-${RUBYVERSION} \
+./configure --prefix=/usr/local \
+    --bindir=/usr/local/bin/ruby-${RUBYVERSION} \
+    --mandir=/usr/local/man \
     && make \
     && make test \
     && sudo make DESTDIR=/stow/ruby-${RUBYVERSION} install
