714dca3dfd2d80da02f7e0db1f9a4fcaa04934f1
Troy Will <troydwill@gmail.com>
2012-01-10 19:17:24 -0800

    modified:   02-install-perl.sh

--- 06-install-perl/02-install-perl.sh
+++ 06-install-perl/02-install-perl.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 VERSION="5.14.2"
-wget www.cpan.org/src/5.0/perl-${VERSION}.tar.bz2
-exit
 sh Configure -de -Dprefix=/usr/local -Dbin=/usr/local/bin/perl-${VERSION}
 make
 make test
 sudo make DESTDIR=/stow/perl-${VERSION} install
-
