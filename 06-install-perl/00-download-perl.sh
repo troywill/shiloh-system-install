684b89ccabc9ec9c4fbd154fb551f7381e282c39
Troy Will <troydwill@gmail.com>
2012-01-10 19:16:06 -0800

    modified:   00-download-perl.sh

--- 06-install-perl/00-download-perl.sh
+++ 06-install-perl/00-download-perl.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-wget www.cpan.org/src/5.0/perl-5.14.2.tar.bz2
-exit
-sh Configure -de
-make
-make test
-sudo make DESTDIR=/stow/perl-5.14.1 install
+VERSION=5.14.2
+wget www.cpan.org/src/5.0/perl-${VERSION}.tar.bz2
