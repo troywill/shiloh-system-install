f834c4b82f8f036a54a143a880c1e8f930f5b99d
Troy Will <troydwill@gmail.com>
2012-01-13 14:12:03 -0800

    new file:   00-installation-scripts/01-erase-drive.sh

---
+++ 00-installation-scripts/01-erase-drive.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# dd if=/dev/zero bs=1M count=1 of=/dev/sda
+# fdisk /dev/sda
+ls -l /dev/disk/by-label
--- 00-installation-scripts/01-erase-sda.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Danger Will Robinson
-dd if=/dev/zero bs=1M count=1 of=/dev/sda
-fdisk /dev/sda
