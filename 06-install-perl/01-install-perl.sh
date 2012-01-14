0f2e0149d0bbbb1758f865fa0c2560f7a0a3c339
Troy Will <troydwill@gmail.com>
2012-01-14 00:02:17 -0800

    modified:   01-install-perl.sh

--- 06-install-perl/01-install-perl.sh
+++ 06-install-perl/01-install-perl.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 VERSION="5.14.2"
 sh Configure -de -Dprefix=/usr/local -Dbin=/usr/local/bin/perl-${VERSION} -Dsitebin=/usr/local/bin/perl-${VERSION}
-make
-make test
-sudo make DESTDIR=/stow/perl-${VERSION} install
+make && make test && sudo make DESTDIR=/stow/perl-${VERSION} install
