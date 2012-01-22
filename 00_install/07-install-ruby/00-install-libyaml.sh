23779d030fedf96833d637a2d403e27be648834c
Troy Will <troydwill@gmail.com>
2012-01-21 21:24:57 -0800

    00-install-libyaml.sh

--- 00_install/07-install-ruby/00-install-libyaml.sh
+++ 00_install/07-install-ruby/00-install-libyaml.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-pacman --sync libyaml
+sudo pacman --sync libyaml
+
 
--- 00_install/07-install-ruby/02-stow-ruby.sh
+++ 00_install/07-install-ruby/02-stow-ruby.sh
@@ -4,9 +4,7 @@ set -o nounset
 
 RUBYVERSION="1.9.3-p0"
 
-./configure --prefix=/usr/local \
-    --bindir=/usr/local/bin/ruby-${RUBYVERSION} \
-    --mandir=/usr/local/man \
-    && make \
-    && make test \
-    && sudo make DESTDIR=/stow/ruby-${RUBYVERSION} install
+./configure --prefix=/usr/local --bindir=/usr/local/bin/ruby-${RUBYVERSION} --mandir=/usr/local/man
+make
+make test
+sudo make DESTDIR=/stow/ruby-${RUBYVERSION} install
