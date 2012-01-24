cd093e8da0a5de5a871a7d6d2b8dc10ac4c086f8
Troy Will <troydwill@gmail.com>
2012-01-23 19:49:38 -0800

    02-stow-ruby.sh

--- 00_install/07-install-ruby/02-stow-ruby.sh
+++ 00_install/07-install-ruby/02-stow-ruby.sh
@@ -7,4 +7,5 @@ RUBYVERSION="1.9.3-p0"
 ./configure --prefix=/usr/local --bindir=/usr/local/bin/ruby-${RUBYVERSION} --mandir=/usr/local/man
 make
 make test
+sudo rm --interactive --recursive /usr/local/lib/ruby
 sudo make DESTDIR=/stow/ruby-${RUBYVERSION} install
