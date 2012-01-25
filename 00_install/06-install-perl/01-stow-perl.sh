85763da8aeae25e0f8758d0ef9d2fb725096c4d8
Troy Will <troydwill@gmail.com>
2012-01-25 09:41:39 -0800

    01-stow-perl.sh

--- 00_install/06-install-perl/01-stow-perl.sh
+++ 00_install/06-install-perl/01-stow-perl.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+set -o errexit
+set -o nounset
+set -o verbose
+
 VERSION="5.14.2"
 sh Configure -de -Dprefix=/usr/local -Dbin=/usr/local/bin/perl-${VERSION} -Dsitebin=/usr/local/bin/perl-${VERSION}
-make && make test && sudo make DESTDIR=/stow/perl-${VERSION} install
+make
+make test
+sudo make DESTDIR=/stow/perl-${VERSION} install
+
