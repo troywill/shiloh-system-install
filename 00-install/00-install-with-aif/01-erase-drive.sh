0e174410eaf17447ca9ad7afe232e5a675bd460d
Troy Will <troydwill@gmail.com>
2012-02-03 09:15:18 -0800

    PROCEDURE: Step by step instructions, Emacs org-mode text file

--- 00-install/00-install-with-aif/01-erase-drive.sh
+++ 00-install/00-install-with-aif/01-erase-drive.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+ls -l /dev/disk/by-label
+
+# The following dd will write zeros to the first Megabyte of your will erase your drive
+# Because of the destructive nature of the command the rest of this file is commented out.
+# Make a copy of this file, uncomment the following three lines, adjust the DRIVE variable to match your target drive
+
+# DRIVE='/dev/sda'
 # dd if=/dev/zero bs=1M count=1 of=/dev/sda
 # fdisk /dev/sda
-ls -l /dev/disk/by-label
--- 00-install/00-install-with-aif/03-link-cache.sh
+++ 00-install/00-install-with-aif/03-link-cache.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Set up symbolic link to source packages
-# Execute this program right before package installation
-
 set -o errexit
 set -o nounset
 
+# This is an optional program
+# Set up symbolic link to source packages
+# Execute this program right before package installation
+
 cd /mnt/var/cache/pacman
-pwd
 mv -vf pkg pkg-hide
 ln -s ../../../../var/cache/pacman/pkg
