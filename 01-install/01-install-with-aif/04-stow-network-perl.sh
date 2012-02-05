40183f13fc3f027371beddfb08eb62985c1419ba
Troy Will <troydwill@gmail.com>
2012-02-04 19:03:51 -0800

    modified:   01-install/01-install-with-aif/04-stow-network-perl.sh

--- 01-install/01-install-with-aif/04-stow-network-perl.sh
+++ 01-install/01-install-with-aif/04-stow-network-perl.sh
@@ -5,5 +5,5 @@ set -o verbose
 
 mkdir --parent --verbose /mnt/stow
 cp --interactive --archive /stow/network-perl /mnt/stow
-cp --interactive stow /mnt/usr/local/bin/
+# cp --interactive stow /mnt/usr/local/bin/
 cp --interactive stowrc /mnt/root/.stowrc
--- 01-install/02-initial-setup/03-automatic-login.sh
+++ 01-install/02-initial-setup/03-automatic-login.sh
@@ -6,7 +6,7 @@ EDITOR='emacs --no-window-system'
 # 2011-05-08 Do Web search on "Automatic login to virtual console"
 # c1:2345:respawn:/sbin/mingetty --autologin USERNAME tty1 linux
 
-cp -iv inittab.troy /etc
-cp -iv inittab.troy /etc/inittab
+cat inittab.troy >> /etc/inittab
 cp --verbose mingetty /sbin
 ${EDITOR} /etc/inittab
+
