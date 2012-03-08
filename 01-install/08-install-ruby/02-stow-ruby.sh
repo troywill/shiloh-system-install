278ae1e4718b6c2dcb2b6dbb96ef433c1b4d9f9a
Troy Will <troydwill@gmail.com>
2012-03-08 15:35:27 -0800

    02-stow-ruby.sh

--- 01-install/08-install-ruby/02-stow-ruby.sh
+++ 01-install/08-install-ruby/02-stow-ruby.sh
@@ -4,15 +4,15 @@ set -o nounset
 set -o verbose
 
 EDITOR=emacs
-RUBYVERSION="1.9.3-p0"
+source ./ruby-version
+# RUBYVERSION="1.9.3-p0"
 PREFIX=/usr/local
 
-./configure --prefix=$PREFIX --bindir=$PREFIX/bin/ruby-$RUBYVERSION --mandir=$PREFIX/man
+./configure --prefix=$PREFIX --bindir=$PREFIX/bin/ruby-$RUBY_VERSION --mandir=$PREFIX/man
 make
-make test
-exit
-sudo make DESTDIR=/stow/ruby-${RUBYVERSION} install
+# make test
+sudo make DESTDIR=/stow/ruby-${RUBY_VERSION} install
 sudo rm --interactive --recursive /usr/local/lib/ruby
-sudo stow ruby-${RUBYVERSION}
+sudo stow ruby-${RUBY_VERSION}
 echo "# /usr/local/bin/ruby-$RUBYVERSION" >> /tmp/tmp_profile
 sudo $EDITOR /etc/profile
