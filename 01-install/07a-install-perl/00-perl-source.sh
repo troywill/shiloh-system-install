641eccaf5841ef59941bf27ddd35f4ff985a1caa
Troy Will <troydwill@gmail.com>
2012-03-18 11:37:34 -0500

    simplelib.sh

--- 01-install/07a-install-perl/00-perl-source.sh
+++ 01-install/07a-install-perl/00-perl-source.sh
@@ -2,6 +2,5 @@
 set -o errexit
 set -o nounset
 
-# 2012-02-03
-VERSION=5.14.2
-wget --no-clobber www.cpan.org/src/5.0/perl-${VERSION}.tar.bz2
+source ../simplelib.sh
+wget --no-clobber www.cpan.org/src/5.0/perl-${PERL_VERSION}.tar.bz2
--- 01-install/simplelib.sh
+++ 01-install/simplelib.sh
@@ -1,6 +1,10 @@
 EDITOR='emacs --no-window-system'
+FILE_DOWNLOAD='wget --no-clobber'
+# Arch/Pacman
 SYSTEM_PACKAGE_INSTALL='sudo pacman --sync'
+# Perl
 CPAN_INSTALL='cpanm --sudo'
+PERL_VERSION='5.14.2'
+# Ruby
 GEM_INSTALL='sudo gem install'
 GEM_OPTIONS='--no-ri --no-rdoc'
-FILE_DOWNLOAD='wget --no-clobber'
