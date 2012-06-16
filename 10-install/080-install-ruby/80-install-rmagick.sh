903a17152e33945d4b0ee010a6cd7804acf50737
Troy Will <troydwill@gmail.com>
2012-06-16 00:10:04 -0700

    new file:   80-install-rmagick.sh

---
+++ 10-install/080-install-ruby/80-install-rmagick.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+wget --no-clobber http://rubyforge.org/frs/download.php/70067/RMagick-2.13.1.tar.bz2
+tar -xf RMagick-2.13.1.tar.bz2
+cd RMagick-2.13.1
+ruby setup.rb
+sudo ruby setup.rb install
+
