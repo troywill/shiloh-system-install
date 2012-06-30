44abeb6713b9226d9a2f4f1ba82fb0d85f45f537
Troy Will <troydwill@gmail.com>
2012-06-29 19:50:09 -0700

    80-install-rmagick.sh: added gsfonts dependency

--- 10-install/085-install-rails/80-install-rmagick.sh
+++ 10-install/085-install-rails/80-install-rmagick.sh
@@ -2,6 +2,8 @@
 set -o errexit
 set -o nounset
 
+sudo pacman --sync gsfonts
+
 wget --no-clobber http://rubyforge.org/frs/download.php/70067/RMagick-2.13.1.tar.bz2
 tar -xf RMagick-2.13.1.tar.bz2
 cd RMagick-2.13.1
