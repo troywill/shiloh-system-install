e9e5e08e6f9b4f7fa6f79bec95b37655a597eb11
Troy Will <troydwill@gmail.com>
2012-11-12 12:02:15 -0800

    new file:   done-todo.pl
    Change DONE to TODO

---
+++ done-todo.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+my $regexp = '\* DONE';
+my $replacement = '* TODO';
+open(my $oldorg, "<", "install-shilohsystem.org")      # open for update
+    or die "Can't open 'install-shilohsystem.org' for reading: $!";
+open(my $neworg, ">", "install-shilohsystem.org.bak")
+    or die "Can't open 'install-shilohsystem.org.bak' for writing: $!";
+while (<$oldorg>) {
+    s/$regexp/$replacement/;
+    print $neworg $_ or die "can't write $neworg: $!";
+}
+
+close($oldorg)            or die "can't close $oldorg: $!";
+close($neworg)            or die "can't close $neworg: $!";
+rename("install-shilohsystem.org.bak", "install-shilohsystem.org")   or die "can't rename install-shilohsystem.org.bak install-shilohsystem.org: $!";
