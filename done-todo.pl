5edc4a7e2953361e392a04f744e6dffbae092880
Troy Will <troydwill@gmail.com>
2013-01-20 11:27:16 -0800

    30-install-cpan-modules: App::cpanoutdated

--- done-todo.pl
+++ done-todo.pl
@@ -12,7 +12,21 @@ while (<$oldorg>) {
     s/$regexp/$replacement/;
     print $neworg $_ or die "can't write $neworg: $!";
 }
+close($oldorg)            or die "can't close $oldorg: $!";
+close($neworg)            or die "can't close $neworg: $!";
+rename("install-shilohsystem.org.bak", "install-shilohsystem.org")   or die "can't rename install-shilohsystem.org.bak install-shilohsystem.org: $!";
 
+##########
+$regexp = '[X]';
+$replacement = '[ ]';
+open($oldorg, "<", "install-shilohsystem.org")      # open for update
+    or die "Can't open 'install-shilohsystem.org' for reading: $!";
+open($neworg, ">", "install-shilohsystem.org.bak")
+    or die "Can't open 'install-shilohsystem.org.bak' for writing: $!";
+while (<$oldorg>) {
+    s/$regexp/$replacement/;
+    print $neworg $_ or die "can't write $neworg: $!";
+}
 close($oldorg)            or die "can't close $oldorg: $!";
 close($neworg)            or die "can't close $neworg: $!";
 rename("install-shilohsystem.org.bak", "install-shilohsystem.org")   or die "can't rename install-shilohsystem.org.bak install-shilohsystem.org: $!";
