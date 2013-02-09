9f5dbd531405f7f4a2e8c86ae78182442b6544ee
Troy Will <troydwill@gmail.com>
2013-02-09 12:56:45 -0800

    screenrc enable-dhcpcd-enp2s1-service.sh

--- services/enable-dhcpcd-enp2s1-service.sh
+++ services/enable-dhcpcd-enp2s1-service.sh
@@ -4,5 +4,9 @@ set -o nounset
 set -o errexit
 set -o verbose
 
-rm -v '/etc/systemd/system/multi-user.target.wants/dhcpcd@eth0.service'
-ln -s '/usr/lib/systemd/system/dhcpcd@.service' '/etc/systemd/system/multi-user.target.wants/dhcpcd@enp2s1.service'
+DEVICE='enp2s1'
+DEVICE='enp0s25'
+
+rm -vf '/etc/systemd/system/multi-user.target.wants/dhcpcd@eth0.service'
+ln -s '/usr/lib/systemd/system/dhcpcd@.service' "/etc/systemd/system/multi-user.target.wants/dhcpcd@${DEVICE}.service"
+
