4c09f6d6026f7cbb3b1067a993f805e32880e610
Troy Will <troydwill@gmail.com>
2013-02-24 18:36:10 -0800

    done-todo.pl: Subroutines

--- done-todo.pl
+++ done-todo.pl
@@ -2,26 +2,47 @@
 
 use warnings;
 use strict;
-my $regexp = '\* DONE';
-my $replacement = '* TODO';
-open(my $oldorg, "<", "install-shilohsystem.org")      # open for update
-    or die "Can't open 'install-shilohsystem.org' for reading: $!";
-open(my $neworg, ">", "install-shilohsystem.org.bak")
-    or die "Can't open 'install-shilohsystem.org.bak' for writing: $!";
-while (<$oldorg>) {
-    s/$regexp/$replacement/;
-    print $neworg $_ or die "can't write $neworg: $!";
-}
-close($oldorg)            or die "can't close $oldorg: $!";
-close($neworg)            or die "can't close $neworg: $!";
-rename("install-shilohsystem.org.bak", "install-shilohsystem.org")   or die "can't rename install-shilohsystem.org.bak install-shilohsystem.org: $!";
+my $infile = 'install-shilohsystem.org';
+my $backup_file = 'install-shilohsystem.org.bak';
 
-##########
-$regexp = '[X]';
+do_replacement( $infile, $backup_file, '\* DONE', '* TODO' );
+do_replacement( $infile, $backup_file, '\[X\]', '[ ]' );
+do_replacement( $infile, $backup_file, '\[\d+\/(\d+)]', '[0\/$1]' );
+
+sub do_replacement {
+    my ( $infile, $backup_file, $regexp, $replacement ) = @_;
+
+    open(my $oldorg, "<", $infile)      # open for update
+    or die "Can't open $infile for reading: $!";
+
+    open(my $neworg, ">", $backup_file)
+    or die "Can't open 'install-shilohsystem.org.bak' for writing: $!";
+
+    while (<$oldorg>) {
+	if ( ($regexp eq '\[\d+\/(\d+)]') && ( $replacement eq '[0\/$1]')) {
+	    s/$regexp/[0\/$1]/;
+	} else {
+	    s/$regexp/$replacement/;
+	}
+	print $neworg $_ or die "can't write $neworg: $!";
+    }
+
+    close($oldorg)            or die "can't close $oldorg: $!";
+    close($neworg)            or die "can't close $neworg: $!";
+    rename($backup_file, $infile)   or die "can't rename install-shilohsystem.org.bak $infile: $!";
+
+}
+
+
+exit;
+__END__
+
+######################################################################
+$regexp = ;
 $replacement = '[ ]';
-open($oldorg, "<", "install-shilohsystem.org")      # open for update
-    or die "Can't open 'install-shilohsystem.org' for reading: $!";
-open($neworg, ">", "install-shilohsystem.org.bak")
+open($oldorg, "<", $infile)      # open for update
+    or die "Can't open $infile for reading: $!";
+open($neworg, ">", $backup_file)
     or die "Can't open 'install-shilohsystem.org.bak' for writing: $!";
 while (<$oldorg>) {
     s/$regexp/$replacement/;
@@ -29,4 +50,21 @@ while (<$oldorg>) {
 }
 close($oldorg)            or die "can't close $oldorg: $!";
 close($neworg)            or die "can't close $neworg: $!";
-rename("install-shilohsystem.org.bak", "install-shilohsystem.org")   or die "can't rename install-shilohsystem.org.bak install-shilohsystem.org: $!";
+rename($backup_file, $infile)   or die "can't rename install-shilohsystem.org.bak $infile: $!";
+
+######################################################################
+$regexp = '\[\d+\/(\d+)]';
+open($oldorg, "<", $infile)      # open for update
+    or die "Can't open $infile for reading: $!";
+open($neworg, ">", $backup_file)
+    or die "Can't open 'install-shilohsystem.org.bak' for writing: $!";
+while (<$oldorg>) {
+    $replacement = '[0\/$1]';
+#    s/$regexp/[0\/$1]/;
+    s/$regexp/$replacement/;
+    print $neworg $_ or die "can't write $neworg: $!";
+}
+close($oldorg)            or die "can't close $oldorg: $!";
+close($neworg)            or die "can't close $neworg: $!";
+rename($backup_file, $infile)   or die "can't rename install-shilohsystem.org.bak $infile: $!";
+
