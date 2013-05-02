4cd693f02dc2bef19c7f5f1ba4818f15b34af80e
Troy Will <troydwill@gmail.com>
2013-05-01 20:01:50 -0700

    modified:   AUR/wget.sh

--- AUR/wget.sh
+++ AUR/wget.sh
@@ -3,10 +3,12 @@ set -o errexit
 set -o nounset
 
 DOWNLOADER="wget --no-clobber"
+BASE_URL="https://aur.archlinux.org/packages"
 
 do_download () {
     $DOWNLOADER $1
 }
 
-do_download "https://aur.archlinux.org/packages/ra/razor-qt/razor-qt.tar.gz"
-do_download "https://aur.archlinux.org/packages/go/google-talkplugin/google-talkplugin.tar.gz"
+do_download "${BASE_URL}/ra/razor-qt/razor-qt.tar.gz"
+do_download "${BASE_URL}/go/google-talkplugin/google-talkplugin.tar.gz"
+do_download "${BASE_URL}/em/emacs-org-mode/emacs-org-mode.tar.gz"
