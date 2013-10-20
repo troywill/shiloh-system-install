4d28431a1608750c14de182b7658cffe24620729
Troy Will <troydwill@gmail.com>
2013-10-20 11:20:32 -0700

    modified:   AUR/aur.org

--- AUR/wget.sh
+++ AUR/wget.sh
@@ -12,3 +12,4 @@ do_download () {
 do_download "${BASE_URL}/ra/razor-qt/razor-qt.tar.gz"
 do_download "${BASE_URL}/go/google-talkplugin/google-talkplugin.tar.gz"
 do_download "${BASE_URL}/em/emacs-org-mode/emacs-org-mode.tar.gz"
+do_download "${BASE_URL}/go/google-earth/google-earth.tar.gz"
