b80e3e7e7ae55a4d6dafd5d545f46f94ad15b300
Troy Will <troydwill@gmail.com>
2013-12-04 18:03:03 -0800

    modified:   20.create-database-cluster.sh

--- APPLICATIONS/POSTGRESQL/20.create-database-cluster.sh
+++ APPLICATIONS/POSTGRESQL/20.create-database-cluster.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 sudo su - postgres --command 'initdb -D "/var/lib/postgres/data"'
+# Success. You can now start the database server using:
+
+#     postgres -D /var/lib/postgres/data
+# or
+#     pg_ctl -D /var/lib/postgres/data -l logfile start
