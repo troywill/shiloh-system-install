4131975858f639cb90ea0988dd1601b0d03c7f3f
Troy Will <troydwill@gmail.com>
2015-07-19 09:37:26 -0700

    install-shilohsystem.org: Scripted the partition setup

--- INSTALLATION/bin/pacstrap.sh
+++ INSTALLATION/bin/pacstrap.sh
@@ -2,11 +2,12 @@
 
 set -o errexit
 set -o nounset
-# February 26, 2015
-# INSTALL="grub-bios os-prober arch-install-scripts parted linux-lts"
-INSTALL="grub-bios os-prober arch-install-scripts parted"
+# July 12, 2015
+# INSTALL="grub os-prober arch-install-scripts parted linux-lts"
+INSTALL="grub os-prober arch-install-scripts parted"
 DEVEL="base-devel git stow cmake"
 SYSTEM="ntp openssh rsync screen sudo wget pkgfile abs"
+SYSTEM="openssh rsync screen sudo wget pkgfile abs"
 EMACS="emacs"
 TEX="texlive-core texlive-latexextra"
 WIRELESS="wireless_tools wpa_supplicant"
@@ -14,7 +15,9 @@ XORG="xorg-server xorg-xinit xf86-video-intel xf86-input-synaptics"
 XORG_FONTS="ttf-dejavu ttf-hannom"
 WEB="chromium firefox"
 KDE="konsole gwenview kdebase-konqueror kstars kdebase-workspace"
-LXQT="openbox"
+# LXQT="openbox"
 AUDIO="alsa-utils pulseaudio-alsa pavucontrol"
 VIDEO="flashplugin youtube-dl smplayer"
-pacstrap /mnt base $INSTALL $DEVEL $SYSTEM $EMACS $WIRELESS $XORG $WEB $KDE $LXQT $AUDIO $VIDEO $XORG_FONTS
+VIDEO="youtube-dl smplayer"
+# pacstrap /mnt base $INSTALL $DEVEL $SYSTEM $EMACS $WIRELESS $XORG $WEB $KDE $LXQT $AUDIO $VIDEO $XORG_FONTS
+pacstrap /mnt base $INSTALL $DEVEL $SYSTEM $EMACS $WIRELESS $XORG $WEB $KDE $AUDIO $VIDEO $XORG_FONTS
--- INSTALLATION/bin/partition/zero-the-drive.sh
+++ INSTALLATION/bin/partition/zero-the-drive.sh
@@ -1,9 +1,33 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 set -o errexit
-DEVICE="/dev/sdb" # find DEVICE with /usr/bin/lsblk and edit this line
-exit
+set -o nounset
+set -o verbose
+
+DEVICE="/dev/sda" # find DEVICE with /usr/bin/lsblk and edit this line
 BS="1M"
 COUNT=4
+# exit
 dd if=${DEVICE} of=SECTOR bs=$BS count=$COUNT
 dd if=/dev/zero of=${DEVICE} bs=$BS count=$COUNT
+
+dd if=${DEVICE} bs=1M count=$COUNT | hexdump -C
+
+
+# Create a GUID partition table with parted
+parted --script ${DEVICE} mklabel gpt
+dd if=${DEVICE} bs=1M count=$COUNT | hexdump -C
+
+
+# Create an EFI partition with parted
+parted --script ${DEVICE} mkpart ESP fat32 1MiB 513MiB set 1 boot on
+
+# Format the boot partition 
+mkfs.vfat -F32 ${DEVICE}1
+
+# Create a root partition with parted
+parted --script ${DEVICE} mkpart primary ext4 513MiB 50%
+
+# Format the root partition
+LABEL="20150718" #change this!
+mkfs.ext4 -v -L $LABEL ${DEVICE}2
